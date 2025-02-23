b57908bc56e2e0c8ca22abb1b1345592b55dec40
jasonqiu0 <jasqiu2@gmail.com>
2025-02-23 16:46:06 +0700

    Update game.js
    make wordsLen vary

--- game.js
+++ game.js
@@ -1,5 +1,6 @@
 const words = 'the this and that with from word but what some can like time just now know take good new come used work long way look very only day use man find here thing give many well want need life call right back mean even name same tell help talk turn move start show play live hold large must big high such follow act why ask keep try last off need put end does set three small own home read hand part place year change much about after before again great where through because different while point group still those leave between old under state run house world let side begin seem next hard open example together white children head story always without grow stop few food land real leave let above kind every around form need air mother father boy girl animal answer learn carry country school plant sentence letter picture soon song river idea city sea second book eye face watch far close night light song leave family door song car dog cat fish bird horse tree star space cloud grass road fire glass stone rain sun moon king queen happy sad fun fast slow big small high low easy hard young old right left hot cold dark light rich poor best worst early late short long new old first last north south east west'.split(' ');
-const wordsLen = words.length;
+//const wordsLen = words.length;
+var wordsLen = 50;
 
 function addClass(element, name) {
     return element.className += ' '+name
