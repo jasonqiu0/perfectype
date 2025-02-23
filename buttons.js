074dc0ead3da7f051a181425f6600c68317efe85
Jason Qiu <jasqiu2@gmail.com>
2025-02-23 20:31:04 +0700

    Add files via upload

---
+++ buttons.js
@@ -0,0 +1,47 @@
+function toggleButton(selector) {
+    const group = document.querySelector(selector);
+    if (!group) return;
+    const buttons = group.querySelectorAll("button");
+    buttons.forEach(button => {
+      button.addEventListener("click", () => {
+        buttons.forEach(btn => btn.classList.remove("active"));
+        button.classList.add("active");
+  
+        console.log(`Button ${button.id} clicked.`);
+      });
+    });
+  }
+  
+  toggleButton("#wordsButtons");
+  toggleButton("#difficultyButtons");
+
+
+document.getElementById("10wordsButton").addEventListener("click", function() {
+    wordsLen = 10;
+    startGame();
+});
+
+document.getElementById("25wordsButton").addEventListener("click", function() {
+    wordsLen = 25;
+    startGame();
+});
+
+document.getElementById("50wordsButton").addEventListener("click", function() {
+    wordsLen = 50;
+    startGame();
+});
+
+document.getElementById("easyButton").addEventListener("click", function() {
+    words = wordsEasy;
+    startGame();
+});
+
+document.getElementById("mediumButton").addEventListener("click", function() {
+    words = wordsMedium;
+    startGame();
+});
+
+document.getElementById("hardButton").addEventListener("click", function() {
+    words = wordsHard;
+    startGame();
+});
