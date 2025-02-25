9efb82050c47a627d49bf5483e95fd5a76aadc85
Jason Qiu <jasqiu2@gmail.com>
2025-02-25 09:52:04 +0700

    Add files via upload

---
+++ timer.js
@@ -0,0 +1,29 @@
+let timeInterval;
+let timeStart = 0;
+let timeStarted = false;
+function startTime() {
+    timeStart = Date.now();
+    timeInterval = setInterval(() => {
+        const elapsed = (Date.now() - timeStart) / 1000;
+        //console.log(elapsed.toFixed(2));
+        document.getElementById("timer").textContent = elapsed.toFixed(2)
+    }, 100);
+}
+
+function stopTime() {
+    clearInterval(timeInterval);
+    timeStarted = false;
+}
+
+function resetTime() {
+    clearInterval(timeInterval);
+    timeStarted = false;
+    document.getElementById("timer").textContent = "0.00";
+  }
+
+document.getElementById("game").addEventListener('keydown', (ev) => {
+    if (!timeStarted && ev.key.length == 1 && ev.key !== ' ') {
+        timeStarted = true;
+        startTime();
+    }
+});
